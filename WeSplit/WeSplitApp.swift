790bd49959d96ec3fcc07f12034fc3fbf5113cc5
Vladislav Shalimov <vshalimov@mindk.com>
2023-12-07 02:44:13 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dexter on 07.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
